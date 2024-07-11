8451191fdc2c9c71cbf86f758c92774f4e0341dd
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-07-11 18:18:12 +0900

    update : enables the creation of multiple qrcodes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import QrCodeWithLogo from 'qrcode-with-logos';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 import './css/reset.css';
 import './css/style.css';
 import LOGO from '../assets/stepmerrily-logo.png';
@@ -6,9 +8,8 @@ import LOGO from '../assets/stepmerrily-logo.png';
 const link = document.querySelector('#link');
 const name = document.querySelector('#name');
 const generateButton = document.querySelector('#generate');
-const canvas = document.querySelector('#canvas');
 
-generateButton.addEventListener('click', () => {
+generateButton.addEventListener('click', async () => {
   if (!link.value.trim()) {
     alert('Please enter a link to generate QR code');
     return false;
@@ -19,18 +20,45 @@ generateButton.addEventListener('click', () => {
     return false;
   }
 
-  const qr = new QrCodeWithLogo({
-    canvas,
-    content: link.value.trim().replace("'", ''),
-    width: 380,
-    logo: { src: LOGO },
-    nodeQrCodeOptions: {},
-    cornersOptions: {},
-    dotsOptions: {},
-    onError: (error) => {
-      console.error(error);
-    },
+  const links = link.value.trim().split('\n');
+  const names = name.value.trim().split('\n');
+
+  if (links.length !== names.length) {
+    alert('Number of links and names must be equal');
+    return false;
+  }
+
+  const zip = new JSZip();
+  const promises = [];
+
+  links.forEach((link, index) => {
+    const qr = new QrCodeWithLogo({
+      content: link.trim().replace("'", ''),
+      width: 380,
+      logo: { src: LOGO },
+      nodeQrCodeOptions: { margin: 0, scale: 0 },
+      cornersOptions: { radius: { outer: 0, inner: 0 } },
+      dotsOptions: {},
+      onError: (error) => {
+        console.error(error);
+      },
+    });
+
+    promises.push(
+      qr.getCanvas().then((canvas) => {
+        return new Promise((resolve) => {
+          canvas.toBlob((blob) => {
+            zip.file(`${names[index].trim()}.png`, blob);
+            resolve();
+          });
+        });
+      })
+    );
   });
 
-  qr.downloadImage(name.value.trim());
+  await Promise.all(promises);
+
+  zip.generateAsync({ type: 'blob' }).then((content) => {
+    saveAs(content, 'qr-codes.zip');
+  });
 });
