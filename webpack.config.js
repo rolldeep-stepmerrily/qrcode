e6db826c803a75b3c5ff4d85c59a119e336a8375
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-07-15 10:02:38 +0900

    refactor : webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,33 +8,16 @@ const __dirname = path.dirname(__filename);
 
 export default {
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
+  output: { path: path.resolve(__dirname, 'dist'), filename: 'bundle.js' },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: './src/css/[name].css',
-    }),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin({ filename: './src/css/[name].css' }),
   ],
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-  },
+  devServer: { static: path.resolve(__dirname, 'dist'), compress: true, port: 9000 },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
     ],
   },
 };
